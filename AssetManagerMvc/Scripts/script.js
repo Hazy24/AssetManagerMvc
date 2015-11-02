e3e2c23453c4ac49660d6c4702000ec57edc3ad7
Hazy24 <hazy24@hotmail.com>
2015-11-02 15:48:01 +0100

    add ability to add new function

---
+++ AssetManagerMvc/Scripts/script.js
@@ -0,0 +1,5 @@
+﻿function changeToText(elementName, linkName) {
+    $('#' + elementName).replaceWith($('<input/>', { 'type': 'text', 'class': 'form-control text-box single-line', 'id': elementName, 'name': elementName }));
+    $('#' + elementName).focus();
+    $('#' + linkName).hide();
+}
