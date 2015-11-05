15f55ebde5158367ce04bb603c9914c663106049
Hazy24 <hazy24@hotmail.com>
2015-11-05 16:39:21 +0100

    added usperiodsprinterscontroller & views, index method & view finished. others todo.

--- AssetManagerMvc/Controllers/UsePeriodsPrintersController.cs
+++ AssetManagerMvc/Controllers/UsePeriodsPrintersController.cs
@@ -10,11 +10,11 @@ using AssetManagerMvc.Models;
 
 namespace AssetManagerMvc.Controllers
 {
-    public class UsePeriodsController : Controller
+    public class UsePeriodsPrintersController : Controller
     {
         private AssetManagerContext db = new AssetManagerContext();
 
-        // GET: UsePeriods
+        // GET: UsePeriodsPrinters
         public ActionResult Index(string sortOrder, string searchString, bool? current,
             bool? hideUitGebruik)
         {
@@ -22,17 +22,18 @@ namespace AssetManagerMvc.Controllers
                 .Include(u => u.Asset)
                 .Include(u => u.Status)
                 .Include(u => u.UserAccount)
-                .Where(u => u.Asset is Computer)
+                .Where(u => u.Asset is Printer)
                 ;
             if ((hideUitGebruik == null) || (hideUitGebruik == true))
             { usePeriods = usePeriods.Where(up => up.Status.UsePeriodStatusId != 4); } // "uit gebruik"
 
-
             if (!String.IsNullOrEmpty(searchString))
             {
                 usePeriods = usePeriods.Where(u => u.UserAccount.Name.Contains(searchString)
-                || (u.Asset as Computer).ComputerName.Contains(searchString)
-                || (u.Asset as Computer).ComputerType.Contains(searchString)
+                || (u.Asset as Printer).PrinterName.Contains(searchString)
+                || (u.Asset as Printer).DrumModel.Contains(searchString)
+                || (u.Asset as Printer).IpAddress.Contains(searchString)
+                || (u.Asset as Printer).TonerModel.Contains(searchString)
                 || u.Asset.Manufacturer.Contains(searchString)
                 || u.Asset.ModelName.Contains(searchString)
                 || u.Asset.SerialNumber.Contains(searchString)
@@ -43,36 +44,32 @@ namespace AssetManagerMvc.Controllers
                 || u.Status.Description.Contains(searchString)
                     );
             }
-
             if ((current == null) || (current == true))
             {
                 usePeriods = usePeriods.Where(up => up.EndDate == null ||
                 up.EndDate >= DateTime.Now);
             }
-
-
             ViewBag.Filter = searchString;
             ViewBag.Current = current;
             ViewBag.HideUitGebruik = hideUitGebruik;
 
             ViewBag.CompoundIdSortParm = String.IsNullOrEmpty(sortOrder) ? "compoundId_desc" : "";
-            ViewBag.ComputerNameSortParm = sortOrder == "computername" ? "computername_desc" : "computername";
+            ViewBag.PrinterNameSortParm = sortOrder == "printername" ? "printername_desc" : "printername";
             ViewBag.SerialNumberSortParm = sortOrder == "serialnumber" ? "serialnumber_desc" : "serialnumber";
             ViewBag.DescriptionSortParm = sortOrder == "description" ? "description_desc" : "description";
             ViewBag.FullNameSortParm = sortOrder == "fullname" ? "fullname_desc" : "fullname";
             ViewBag.FunctionSortParm = sortOrder == "function" ? "function_desc" : "function";
-
 
             switch (sortOrder)
             {
                 case "compoundId_desc":
                     usePeriods = usePeriods.OrderByDescending(u => u.AssetId);
                     break;
-                case "computername":
-                    usePeriods = usePeriods.OrderBy(u => (u.Asset as Computer).ComputerName);
+                case "printername":
+                    usePeriods = usePeriods.OrderBy(u => (u.Asset as Printer).PrinterName);
                     break;
-                case "computername_desc":
-                    usePeriods = usePeriods.OrderByDescending(u => (u.Asset as Computer).ComputerName);
+                case "printername_desc":
+                    usePeriods = usePeriods.OrderByDescending(u => (u.Asset as Printer).PrinterName);
                     break;
                 case "serialnumber":
                     usePeriods = usePeriods.OrderBy(u => u.Asset.SerialNumber);
@@ -103,10 +100,11 @@ namespace AssetManagerMvc.Controllers
                     break;
             }
 
+
             return View(usePeriods.ToList());
         }
 
-        // GET: UsePeriods/Details/5
+        // GET: UsePeriodsPrinters/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -121,33 +119,21 @@ namespace AssetManagerMvc.Controllers
             return View(usePeriod);
         }
 
-        // GET: UsePeriods/Create       
-        public ActionResult Create(int? oldUsePeriodId)
+        // GET: UsePeriodsPrinters/Create
+        public ActionResult Create()
         {
-            UsePeriod up = new UsePeriod();
-            up.StartDate = DateTime.Now;
-
-            if (oldUsePeriodId != null)
-            {
-                UsePeriod oldUsePeriod = db.UsePeriods.Single(oup => oup.UsePeriodId == oldUsePeriodId);
-                oldUsePeriod.EndDate = DateTime.Today;
-                db.SaveChanges();
-                SetCreateAndEditViewbag(oldUsePeriod.AssetId);
-            }
-            else
-            {
-                SetCreateAndEditViewbag();
-            }
-            return View(up);
+            ViewBag.AssetId = new SelectList(db.Assets, "AssetId", "SerialNumber");
+            ViewBag.UsePeriodStatusId = new SelectList(db.UsePeriodStatuses, "UsePeriodStatusId", "Description");
+            ViewBag.UserAccountId = new SelectList(db.UserAccounts, "UserAccountId", "Name");
+            return View();
         }
 
-
-        // POST: UsePeriods/Create
+        // POST: UsePeriodsPrinters/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "UsePeriodId,UserAccountId,StartDate,EndDate,Remark,Function,UsePeriodStatusId,AssetId,UserIsAdmin")] UsePeriod usePeriod)
+        public ActionResult Create([Bind(Include = "UsePeriodId,UserAccountId,StartDate,EndDate,Remark,Function,UserIsAdmin,UsePeriodStatusId,AssetId")] UsePeriod usePeriod)
         {
             if (ModelState.IsValid)
             {
@@ -156,12 +142,13 @@ namespace AssetManagerMvc.Controllers
                 return RedirectToAction("Index");
             }
 
-            SetCreateAndEditViewbag(usePeriod.AssetId, usePeriod.UsePeriodStatusId,
-                usePeriod.UserAccountId, usePeriod.Function);
+            ViewBag.AssetId = new SelectList(db.Assets, "AssetId", "SerialNumber", usePeriod.AssetId);
+            ViewBag.UsePeriodStatusId = new SelectList(db.UsePeriodStatuses, "UsePeriodStatusId", "Description", usePeriod.UsePeriodStatusId);
+            ViewBag.UserAccountId = new SelectList(db.UserAccounts, "UserAccountId", "Name", usePeriod.UserAccountId);
             return View(usePeriod);
         }
 
-        // GET: UsePeriods/Edit/5
+        // GET: UsePeriodsPrinters/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -173,18 +160,18 @@ namespace AssetManagerMvc.Controllers
             {
                 return HttpNotFound();
             }
-            SetCreateAndEditViewbag(usePeriod.AssetId, usePeriod.UsePeriodStatusId,
-                usePeriod.UserAccountId, usePeriod.Function);
-
+            ViewBag.AssetId = new SelectList(db.Assets, "AssetId", "SerialNumber", usePeriod.AssetId);
+            ViewBag.UsePeriodStatusId = new SelectList(db.UsePeriodStatuses, "UsePeriodStatusId", "Description", usePeriod.UsePeriodStatusId);
+            ViewBag.UserAccountId = new SelectList(db.UserAccounts, "UserAccountId", "Name", usePeriod.UserAccountId);
             return View(usePeriod);
         }
 
-        // POST: UsePeriods/Edit/5
+        // POST: UsePeriodsPrinters/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "UsePeriodId,UserAccountId,StartDate,EndDate,Remark,Function,UsePeriodStatusId,AssetId,UserIsAdmin")] UsePeriod usePeriod)
+        public ActionResult Edit([Bind(Include = "UsePeriodId,UserAccountId,StartDate,EndDate,Remark,Function,UserIsAdmin,UsePeriodStatusId,AssetId")] UsePeriod usePeriod)
         {
             if (ModelState.IsValid)
             {
@@ -192,53 +179,13 @@ namespace AssetManagerMvc.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            SetCreateAndEditViewbag(usePeriod.AssetId, usePeriod.UsePeriodStatusId,
-                  usePeriod.UserAccountId, usePeriod.Function);
+            ViewBag.AssetId = new SelectList(db.Assets, "AssetId", "SerialNumber", usePeriod.AssetId);
+            ViewBag.UsePeriodStatusId = new SelectList(db.UsePeriodStatuses, "UsePeriodStatusId", "Description", usePeriod.UsePeriodStatusId);
+            ViewBag.UserAccountId = new SelectList(db.UserAccounts, "UserAccountId", "Name", usePeriod.UserAccountId);
             return View(usePeriod);
         }
-        private void SetCreateAndEditViewbag(int? assetId = null, int? usePeriodStatusId = null,
-            int? userAccountId = null, string function = null)
-        {
-            if (assetId == null)
-            {
-                ViewBag.AssetId = new SelectList(db.Assets, "AssetId", "CompoundIdAndSerialNumber");
-            }
-            else
-            {
-                ViewBag.AssetId = new SelectList(db.Assets, "AssetId", "CompoundIdAndSerialNumber", assetId);
-            }
-            if (usePeriodStatusId == null)
-            {
-                ViewBag.UsePeriodStatusId = new SelectList(db.UsePeriodStatuses, "UsePeriodStatusId", "Description");
-            }
-            else
-            {
-                ViewBag.UsePeriodStatusId = new SelectList(db.UsePeriodStatuses, "UsePeriodStatusId", "Description", usePeriodStatusId);
-            }
-            if (userAccountId == null)
-            {
-                ViewBag.UserAccountId = new SelectList(db.UserAccounts.OrderBy(x => x.Name), "UserAccountId", "Name").ToList();
-            }
-            else
-            {
-                ViewBag.UserAccountId = new SelectList(db.UserAccounts.OrderBy(x => x.Name), "UserAccountId", "Name", userAccountId).ToList();
-            }
-            ViewBag.UserAccountId.Insert(0, new SelectListItem { Text = "", Value = "" });
 
-            if (string.IsNullOrEmpty(function))
-            {
-                ViewBag.Function = new SelectList(db.UsePeriods, "Function", "Function")
-                .GroupBy(f => f.Text).Select(f => f.First()) // == Distinct              
-                .OrderBy(f => f.Text);
-            }
-            else
-            {
-                ViewBag.Function = new SelectList(db.UsePeriods, "Function", "Function", function)
-                .GroupBy(f => f.Text).Select(f => f.First()) // == Distinct              
-                .OrderBy(f => f.Text);
-            }
-        }
-        // GET: UsePeriods/Delete/5
+        // GET: UsePeriodsPrinters/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -253,7 +200,7 @@ namespace AssetManagerMvc.Controllers
             return View(usePeriod);
         }
 
-        // POST: UsePeriods/Delete/5
+        // POST: UsePeriodsPrinters/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
