dde44c4da3dfdee605784385ef45b2c4eeb8d0b1
Hazy24 <hazy24@hotmail.com>
2015-11-18 09:33:26 +0100

    make ip clickable in printers index

--- AssetManagerMvc/Models/Telephone.cs
+++ AssetManagerMvc/Models/Telephone.cs
@@ -11,7 +11,8 @@ namespace AssetManagerMvc.Models
         [Display(Name = "Type")]
         public string TelephoneType { get; set; }
 
-        [RegularExpression(@"0[0-9]\s[0-9][0-9][0-9]\s[0-9][0-9]\s[0-9][0-9]|0[0-9][0-9][0-9]\s[0-9][0-9]\s[0-9][0-9]\s[0-9][0-9]", ErrorMessage = "Use e.g. 09 123 45 67 or 0485 12 34 56")]
+        [RegularExpression(@"0[0-9]\s[0-9][0-9][0-9]\s[0-9][0-9]\s[0-9][0-9]|0[0-9][0-9][0-9]\s[0-9][0-9]\s[0-9][0-9]\s[0-9][0-9]", 
+            ErrorMessage = "Use e.g. 09 123 45 67 or 0485 12 34 56")]
         public string Number { get; set; }
 
         [RegularExpression(@"[0-9][0-9][0-9]", ErrorMessage = "Use e.g. 123")]
