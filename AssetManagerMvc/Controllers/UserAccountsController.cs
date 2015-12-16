f796290613621ae8b5daa8bd51ca730bea9005ee
Hazy24 <hazy24@hotmail.com>
2015-12-16 16:17:06 +0100

    compound id link in overzicht useperiods in useraccount detail

--- AssetManagerMvc/Controllers/UserAccountsController.cs
+++ AssetManagerMvc/Controllers/UserAccountsController.cs
@@ -110,7 +110,7 @@ namespace AssetManagerMvc.Controllers
         }
         // GET: UserAccounts/Details/5
         public ActionResult Details(int? id)
-        {            
+        {                 
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
