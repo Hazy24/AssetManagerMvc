8c2f65b71752e7870fc9fc32838dbe73de3cf6eb
Hazy24 <hazy24@hotmail.com>
2015-12-23 16:26:35 +0100

    filter telephonelist

--- AssetManagerMvc/Controllers/TelephoneListController.cs
+++ AssetManagerMvc/Controllers/TelephoneListController.cs
@@ -19,22 +19,38 @@ namespace AssetManagerMvc.Controllers
         private AssetManagerContext db = new AssetManagerContext();
 
         // GET: TelephoneList
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string searchString)
         {
-            List<TelephoneListItem> list = InternWithoutLog();
-            list.AddRange(ExternLog());
-            list.AddRange(GSM());
-            list.AddRange(WithFunction());
-            return View(list);
+
+            List<TelephoneListItem> telephoneList = InternWithoutLog().ToList();
+            telephoneList.AddRange(ExternLog());
+            telephoneList.AddRange(GSM());
+            telephoneList.AddRange(WithFunction());            
+
+            if (!String.IsNullOrWhiteSpace(searchString))
+            {
+                telephoneList = telephoneList.Where
+                    (t => (!string.IsNullOrEmpty(t.Name) 
+                      && t.Name.ToLower().Contains(searchString.ToLower()))
+                      || (!string.IsNullOrEmpty(t.Department)
+                      && t.Department.ToLower().Contains(searchString.ToLower()))
+                      || (!string.IsNullOrEmpty(t.Number)
+                      && t.Number.Contains(searchString.ToLower()))
+                      || (!string.IsNullOrEmpty(t.NumberIntern)
+                      && t.NumberIntern.Contains(searchString.ToLower()))
+                    )
+                .ToList();
+            }
+            return View(telephoneList);
         }
 
         public ActionResult Print()
         {
             return File(TelephoneListPDFStream(), "application/pdf", "telefoonlijst.pdf");
         }
-        private List<TelephoneListItem> InternWithoutLog()
+        private IQueryable<TelephoneListItem> InternWithoutLog()
         {
-            List<TelephoneListItem> list = db.UsePeriods
+            var query = db.UsePeriods
                .Include(u => u.Asset)
                .Include(u => u.Status)
                .Include(u => u.UserAccount)              
@@ -52,12 +68,12 @@ namespace AssetManagerMvc.Controllers
                    Number = (u.Asset as Telephone).Number,
                    NumberIntern = (u.Asset as Telephone).NumberIntern
                })
-               .ToList();
-            return list;
+               ;
+            return query;
         }
-        private List<TelephoneListItem> ExternLog()
+        private IQueryable<TelephoneListItem> ExternLog()
         {
-            List<TelephoneListItem> list = db.UsePeriods
+            IQueryable<TelephoneListItem> query = db.UsePeriods
                .Include(u => u.Asset)
                .Include(u => u.Status)
                .Include(u => u.UserAccount)               
@@ -75,12 +91,12 @@ namespace AssetManagerMvc.Controllers
                    Number = (u.Asset as Telephone).Number,
                    NumberIntern = (u.Asset as Telephone).NumberIntern
                })
-               .ToList();
-            return list;
+               ;
+            return query;
         }
-        private List<TelephoneListItem> GSM()
+        private IQueryable<TelephoneListItem> GSM()
         {
-            List<TelephoneListItem> list = db.UsePeriods
+            IQueryable<TelephoneListItem> query = db.UsePeriods
                .Include(u => u.Asset)
                .Include(u => u.Status)
                .Include(u => u.UserAccount)               
@@ -93,15 +109,16 @@ namespace AssetManagerMvc.Controllers
                .Select(u => new TelephoneListItem
                {                   
                    Name = u.UserAccount.Name,
+                   Department = u.UserAccount.Department.Name,
                    Number = (u.Asset as Telephone).Number,
                    NumberIntern = (u.Asset as Telephone).NumberIntern
                })
-               .ToList();
-            return list;
+               ;
+            return query;
         }
-        private List<TelephoneListItem> WithFunction()
+        private IQueryable<TelephoneListItem> WithFunction()
         {
-            List<TelephoneListItem> list = db.UsePeriods
+            IQueryable<TelephoneListItem> query = db.UsePeriods
                .Include(u => u.Asset)
                .Include(u => u.Status)
                .Include(u => u.UserAccount)               
@@ -114,12 +131,13 @@ namespace AssetManagerMvc.Controllers
                .Select(u => new TelephoneListItem
                {                   
                    Name = u.Function,
+                   Department = u.UserAccount.Department.Name,
                    Number = (u.Asset as Telephone).Number,
                    NumberIntern = (u.Asset as Telephone).NumberIntern
                })
-               .ToList();
+               ;
 
-            return list;
+            return query;
         }
         private MemoryStream TelephoneListPDFStream()
         {
@@ -129,6 +147,7 @@ namespace AssetManagerMvc.Controllers
             try
             {
                 PdfWriter pdfWriter = PdfWriter.GetInstance(document, stream);
+                // pdfWriter.PageEvent = new ITextEvents();
                 pdfWriter.CloseStream = false;
                 document.Open();
 
@@ -181,10 +200,191 @@ namespace AssetManagerMvc.Controllers
 
             return stream;
         }
+        public class ITextEvents : PdfPageEventHelper
+        {
 
+            // This is the contentbyte object of the writer
+            PdfContentByte cb;
+
+            // we will put the final number of pages in a template
+            PdfTemplate headerTemplate, footerTemplate;
+
+            // this is the BaseFont we are going to use for the header / footer
+            BaseFont bf = null;
+
+            // This keeps track of the creation time
+            DateTime PrintTime = DateTime.Now;
+
+
+            #region Fields
+            private string _header;
+            #endregion
+
+            #region Properties
+            public string Header
+            {
+                get { return _header; }
+                set { _header = value; }
+            }
+            #endregion
+
+
+            public override void OnOpenDocument(PdfWriter writer, Document document)
+            {
+                try
+                {
+                    PrintTime = DateTime.Now;
+                    bf = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+                    cb = writer.DirectContent;
+                    headerTemplate = cb.CreateTemplate(100, 100);
+                    footerTemplate = cb.CreateTemplate(50, 50);
+                }
+                catch (DocumentException de)
+                {
+
+                }
+                catch (System.IO.IOException ioe)
+                {
+
+                }
+            }
+
+            public override void OnEndPage(iTextSharp.text.pdf.PdfWriter writer, iTextSharp.text.Document document)
+            {
+                base.OnEndPage(writer, document);
+
+                iTextSharp.text.Font baseFontNormal = new iTextSharp.text.Font(iTextSharp.text.Font.HELVETICA, 12f, iTextSharp.text.Font.NORMAL);
+
+                iTextSharp.text.Font baseFontBig = new iTextSharp.text.Font(iTextSharp.text.Font.HELVETICA, 12f, iTextSharp.text.Font.BOLD);
+
+                Phrase p1Header = new Phrase("Sample Header Here", baseFontNormal);
+
+                //Create PdfTable object
+                PdfPTable pdfTab = new PdfPTable(3);
+
+                //We will have to create separate cells to include image logo and 2 separate strings
+                //Row 1
+                PdfPCell pdfCell1 = new PdfPCell();
+                PdfPCell pdfCell2 = new PdfPCell(p1Header);
+                PdfPCell pdfCell3 = new PdfPCell();
+                String text = "Page " + writer.PageNumber + " of ";
+
+
+                //Add paging to header
+                {
+                    cb.BeginText();
+                    cb.SetFontAndSize(bf, 12);
+                    cb.SetTextMatrix(document.PageSize.GetRight(200), document.PageSize.GetTop(45));
+                    cb.ShowText(text);
+                    cb.EndText();
+                    float len = bf.GetWidthPoint(text, 12);
+                    //Adds "12" in Page 1 of 12
+                    cb.AddTemplate(headerTemplate, document.PageSize.GetRight(200) + len, document.PageSize.GetTop(45));
+                }
+                //Add paging to footer
+                {
+                    cb.BeginText();
+                    cb.SetFontAndSize(bf, 12);
+                    cb.SetTextMatrix(document.PageSize.GetRight(180), document.PageSize.GetBottom(30));
+                    cb.ShowText(text);
+                    cb.EndText();
+                    float len = bf.GetWidthPoint(text, 12);
+                    cb.AddTemplate(footerTemplate, document.PageSize.GetRight(180) + len, document.PageSize.GetBottom(30));
+                }
+                //Row 2
+                PdfPCell pdfCell4 = new PdfPCell(new Phrase("Sub Header Description", baseFontNormal));
+                //Row 3
+
+
+                PdfPCell pdfCell5 = new PdfPCell(new Phrase("Date:" + PrintTime.ToShortDateString(), baseFontBig));
+                PdfPCell pdfCell6 = new PdfPCell();
+                PdfPCell pdfCell7 = new PdfPCell(new Phrase("TIME:" + string.Format("{0:t}", DateTime.Now), baseFontBig));
+
+
+                //set the alignment of all three cells and set border to 0
+                pdfCell1.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell2.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell3.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell4.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell5.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell6.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell7.HorizontalAlignment = Element.ALIGN_CENTER;
+
+
+                pdfCell2.VerticalAlignment = Element.ALIGN_BOTTOM;
+                pdfCell3.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell4.VerticalAlignment = Element.ALIGN_TOP;
+                pdfCell5.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell6.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell7.VerticalAlignment = Element.ALIGN_MIDDLE;
+
+
+                pdfCell4.Colspan = 3;
+
+
+
+                pdfCell1.Border = 0;
+                pdfCell2.Border = 0;
+                pdfCell3.Border = 0;
+                pdfCell4.Border = 0;
+                pdfCell5.Border = 0;
+                pdfCell6.Border = 0;
+                pdfCell7.Border = 0;
+
+
+                //add all three cells into PdfTable
+                pdfTab.AddCell(pdfCell1);
+                pdfTab.AddCell(pdfCell2);
+                pdfTab.AddCell(pdfCell3);
+                pdfTab.AddCell(pdfCell4);
+                pdfTab.AddCell(pdfCell5);
+                pdfTab.AddCell(pdfCell6);
+                pdfTab.AddCell(pdfCell7);
+
+                pdfTab.TotalWidth = document.PageSize.Width - 80f;
+                pdfTab.WidthPercentage = 70;
+                //pdfTab.HorizontalAlignment = Element.ALIGN_CENTER;
+
+
+                //call WriteSelectedRows of PdfTable. This writes rows from PdfWriter in PdfTable
+                //first param is start row. -1 indicates there is no end row and all the rows to be included to write
+                //Third and fourth param is x and y position to start writing
+                pdfTab.WriteSelectedRows(0, -1, 40, document.PageSize.Height - 30, writer.DirectContent);
+                //set pdfContent value
+
+                //Move the pointer and draw line to separate header section from rest of page
+                cb.MoveTo(40, document.PageSize.Height - 100);
+                cb.LineTo(document.PageSize.Width - 40, document.PageSize.Height - 100);
+                cb.Stroke();
+
+                //Move the pointer and draw line to separate footer section from rest of page
+                cb.MoveTo(40, document.PageSize.GetBottom(50));
+                cb.LineTo(document.PageSize.Width - 40, document.PageSize.GetBottom(50));
+                cb.Stroke();
+            }
+
+            public override void OnCloseDocument(PdfWriter writer, Document document)
+            {
+                base.OnCloseDocument(writer, document);
+
+                headerTemplate.BeginText();
+                headerTemplate.SetFontAndSize(bf, 12);
+                headerTemplate.SetTextMatrix(0, 0);
+                headerTemplate.ShowText((writer.PageNumber - 1).ToString());
+                headerTemplate.EndText();
+
+                footerTemplate.BeginText();
+                footerTemplate.SetFontAndSize(bf, 12);
+                footerTemplate.SetTextMatrix(0, 0);
+                footerTemplate.ShowText((writer.PageNumber - 1).ToString());
+                footerTemplate.EndText();
+
+
+            }
+        }
         private IElement GetPdfPtableGSMAndFunction(int rowsPerPage)
         {
-            List<TelephoneListItem> lstGSM = GSM();
+            List<TelephoneListItem> lstGSM = GSM().ToList();
             // lstGSM.Add(new TelephoneListItem { Name = "Extra GSM", Number = "0456 78 90 12" });
             PdfPTable tableGSMAndFunction = new PdfPTable(2);            
             tableGSMAndFunction.WidthPercentage = 100f;
@@ -214,7 +414,7 @@ namespace AssetManagerMvc.Controllers
             cell.Border = Rectangle.TOP_BORDER;
             tableGSMAndFunction.AddCell(cell);
 
-            List<TelephoneListItem> lstFunction = WithFunction();
+            List<TelephoneListItem> lstFunction = WithFunction().ToList();
             for (int i = 0; i < lstFunction.Count; i++)
             {
                 TelephoneListItem number = lstFunction[i];
@@ -251,7 +451,7 @@ namespace AssetManagerMvc.Controllers
 
         private PdfPTable GetPdfPtableExternLog()
         {
-            List<TelephoneListItem> lstExternLog = ExternLog();
+            List<TelephoneListItem> lstExternLog = ExternLog().ToList();
             PdfPTable tblExternLog = new PdfPTable(2);
             tblExternLog.SpacingBefore = 15f;
             //float[] widths = new float[] { 3f, 1f, 3f };
@@ -280,7 +480,7 @@ namespace AssetManagerMvc.Controllers
 
         private PdfPTable GetPdfPtableInternWithoutLog(int rowsPerPage)
         {
-            List<TelephoneListItem> lstInternWithoutLog = InternWithoutLog();
+            List<TelephoneListItem> lstInternWithoutLog = InternWithoutLog().ToList();
             PdfPTable tblInternWithoutLog = new PdfPTable(3);
             tblInternWithoutLog.SpacingAfter = 15f;
             float[] widths = new float[] { 3f, 1f, 3f };
