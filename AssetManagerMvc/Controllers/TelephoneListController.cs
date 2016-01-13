0e8ca0feabdba85cc02d6e3d2358ec777adffb74
Hazy24 <hazy24@hotmail.com>
2016-01-13 12:18:06 +0100

    add date to telephonelist

--- AssetManagerMvc/Controllers/TelephoneListController.cs
+++ AssetManagerMvc/Controllers/TelephoneListController.cs
@@ -170,16 +170,13 @@ namespace AssetManagerMvc.Controllers
             try
             {
                 PdfWriter pdfWriter = PdfWriter.GetInstance(document, stream);
-                // pdfWriter.PageEvent = new ITextEvents();
+                pdfWriter.PageEvent = new ITextEvents();
                 pdfWriter.CloseStream = false;
                 document.Open();
 
                 string fontpath = Server.MapPath(@"~/fonts/");
                 BaseFont OxfamGlobalHeadline = BaseFont.CreateFont(fontpath + "OxfamGlobalHeadline.ttf", 
-                    BaseFont.CP1252, BaseFont.EMBEDDED);
-                //Font font = new Font(customfont, 12);
-                //string s = "My expensive custom font.";
-                //document.Add(new Paragraph(s, font));
+                    BaseFont.CP1252, BaseFont.EMBEDDED);                
 
                 Paragraph heading = new Paragraph("TELEFOONLIJST", new Font(OxfamGlobalHeadline, 28f, Font.BOLD));
                 heading.SpacingAfter = 40f;
@@ -258,6 +255,7 @@ namespace AssetManagerMvc.Controllers
                 {
                     PrintTime = DateTime.Now;
                     bf = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+                    
                     cb = writer.DirectContent;
                     headerTemplate = cb.CreateTemplate(100, 100);
                     footerTemplate = cb.CreateTemplate(50, 50);
@@ -276,9 +274,9 @@ namespace AssetManagerMvc.Controllers
             {
                 base.OnEndPage(writer, document);
 
-                iTextSharp.text.Font baseFontNormal = new iTextSharp.text.Font(iTextSharp.text.Font.HELVETICA, 12f, iTextSharp.text.Font.NORMAL);
+                iTextSharp.text.Font baseFontNormal = new iTextSharp.text.Font(iTextSharp.text.Font.HELVETICA, 9f, iTextSharp.text.Font.NORMAL);
 
-                iTextSharp.text.Font baseFontBig = new iTextSharp.text.Font(iTextSharp.text.Font.HELVETICA, 12f, iTextSharp.text.Font.BOLD);
+                iTextSharp.text.Font baseFontBig = new iTextSharp.text.Font(iTextSharp.text.Font.HELVETICA, 9f, iTextSharp.text.Font.BOLD);
 
                 Phrase p1Header = new Phrase("Sample Header Here", baseFontNormal);
 
@@ -288,40 +286,42 @@ namespace AssetManagerMvc.Controllers
                 //We will have to create separate cells to include image logo and 2 separate strings
                 //Row 1
                 PdfPCell pdfCell1 = new PdfPCell();
-                PdfPCell pdfCell2 = new PdfPCell(p1Header);
+                // PdfPCell pdfCell2 = new PdfPCell(p1Header);
+                PdfPCell pdfCell2 = new PdfPCell();
                 PdfPCell pdfCell3 = new PdfPCell();
                 String text = "Page " + writer.PageNumber + " of ";
 
 
                 //Add paging to header
                 {
-                    cb.BeginText();
-                    cb.SetFontAndSize(bf, 12);
-                    cb.SetTextMatrix(document.PageSize.GetRight(200), document.PageSize.GetTop(45));
-                    cb.ShowText(text);
-                    cb.EndText();
-                    float len = bf.GetWidthPoint(text, 12);
-                    //Adds "12" in Page 1 of 12
-                    cb.AddTemplate(headerTemplate, document.PageSize.GetRight(200) + len, document.PageSize.GetTop(45));
+                    //cb.BeginText();
+                    //cb.SetFontAndSize(bf, 9);
+                    //cb.SetTextMatrix(document.PageSize.GetRight(200), document.PageSize.GetTop(45));
+                    //cb.ShowText(text);
+                    //cb.EndText();
+                    //float len = bf.GetWidthPoint(text, 9);
+                    ////Adds "12" in Page 1 of 12
+                    //cb.AddTemplate(headerTemplate, document.PageSize.GetRight(200) + len, document.PageSize.GetTop(45));
                 }
                 //Add paging to footer
                 {
-                    cb.BeginText();
-                    cb.SetFontAndSize(bf, 12);
-                    cb.SetTextMatrix(document.PageSize.GetRight(180), document.PageSize.GetBottom(30));
-                    cb.ShowText(text);
-                    cb.EndText();
-                    float len = bf.GetWidthPoint(text, 12);
-                    cb.AddTemplate(footerTemplate, document.PageSize.GetRight(180) + len, document.PageSize.GetBottom(30));
+                    //cb.BeginText();
+                    //cb.SetFontAndSize(bf, 12);
+                    //cb.SetTextMatrix(document.PageSize.GetRight(180), document.PageSize.GetBottom(30));
+                    //cb.ShowText(text);
+                    //cb.EndText();
+                    //float len = bf.GetWidthPoint(text, 12);
+                    //cb.AddTemplate(footerTemplate, document.PageSize.GetRight(180) + len, document.PageSize.GetBottom(30));
                 }
                 //Row 2
-                PdfPCell pdfCell4 = new PdfPCell(new Phrase("Sub Header Description", baseFontNormal));
+                // PdfPCell pdfCell4 = new PdfPCell(new Phrase("Sub Header Description", baseFontNormal));
+                PdfPCell pdfCell4 = new PdfPCell();
                 //Row 3
 
 
-                PdfPCell pdfCell5 = new PdfPCell(new Phrase("Date:" + PrintTime.ToShortDateString(), baseFontBig));
+                PdfPCell pdfCell5 = new PdfPCell();
                 PdfPCell pdfCell6 = new PdfPCell();
-                PdfPCell pdfCell7 = new PdfPCell(new Phrase("TIME:" + string.Format("{0:t}", DateTime.Now), baseFontBig));
+                PdfPCell pdfCell7 = new PdfPCell(new Phrase(PrintTime.ToShortDateString(), baseFontNormal));
 
 
                 //set the alignment of all three cells and set border to 0
@@ -331,7 +331,7 @@ namespace AssetManagerMvc.Controllers
                 pdfCell4.HorizontalAlignment = Element.ALIGN_CENTER;
                 pdfCell5.HorizontalAlignment = Element.ALIGN_CENTER;
                 pdfCell6.HorizontalAlignment = Element.ALIGN_CENTER;
-                pdfCell7.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell7.HorizontalAlignment = Element.ALIGN_RIGHT;
 
 
                 pdfCell2.VerticalAlignment = Element.ALIGN_BOTTOM;
@@ -376,14 +376,14 @@ namespace AssetManagerMvc.Controllers
                 //set pdfContent value
 
                 //Move the pointer and draw line to separate header section from rest of page
-                cb.MoveTo(40, document.PageSize.Height - 100);
-                cb.LineTo(document.PageSize.Width - 40, document.PageSize.Height - 100);
-                cb.Stroke();
+                //cb.MoveTo(40, document.PageSize.Height - 100);
+                //cb.LineTo(document.PageSize.Width - 40, document.PageSize.Height - 100);
+                //cb.Stroke();
 
                 //Move the pointer and draw line to separate footer section from rest of page
-                cb.MoveTo(40, document.PageSize.GetBottom(50));
-                cb.LineTo(document.PageSize.Width - 40, document.PageSize.GetBottom(50));
-                cb.Stroke();
+                //cb.MoveTo(40, document.PageSize.GetBottom(50));
+                //cb.LineTo(document.PageSize.Width - 40, document.PageSize.GetBottom(50));
+                //cb.Stroke();
             }
 
             public override void OnCloseDocument(PdfWriter writer, Document document)
@@ -534,202 +534,7 @@ namespace AssetManagerMvc.Controllers
                 tblInternWithoutLog.AddCell(cell);
             }
             return tblInternWithoutLog;
-        }
-
-        private MemoryStream TelephoneListPDFStreamOld()
-        {
-            MemoryStream stream = new MemoryStream();
-            Document document = new Document();
-            List<Group<string, TelephoneListItem>> listByDep = TelephoneListByDepartment();
-            List<TelephoneListItem> listLog = TelephoneListLog(db);
-            List<TelephoneListItem> listGSM = TelephoneListGSM(db);
-            // Font font = FontFactory.GetFont("Arial", 28, Color.BLACK);          
-
-            try
-            {
-                PdfWriter pdfWriter = PdfWriter.GetInstance(document, stream);
-                pdfWriter.CloseStream = false;
-                document.Open();
-
-
-                PdfPTable layoutTable = new PdfPTable(2);
-                layoutTable.WidthPercentage = 100f;
-                layoutTable.SplitLate = false;
-
-                PdfPCell cellTitle = CreateCell(5f, "Telefoonlijst");
-                cellTitle.Colspan = 2;
-                cellTitle.HorizontalAlignment = 1; // centered
-                layoutTable.AddCell(cellTitle);
-
-                PdfPTable tblLeft = new PdfPTable(1);
-
-                PdfPCell cellIntern = CreateCellWithParaTitle(5f, "Interne nummers");
-
-                foreach (var dep in listByDep)
-                {
-                    Paragraph para = new Paragraph();
-                    para.Add(dep.Key);
-                    para.SpacingAfter = 3f;
-                    cellIntern.AddElement(para);
-
-                    PdfPTable table = depTable();
-
-                    foreach (var number in dep.Values)
-                    {
-                        PdfPCell cell = new PdfPCell(new Phrase(number.Name));
-                        // cell.Border = Rectangle.BOTTOM_BORDER | Rectangle.TOP_BORDER | Rectangle.LEFT_BORDER;
-                        table.AddCell(cell);
-                        cell = new PdfPCell(new Phrase(number.NumberIntern));
-                        // cell.Border = Rectangle.BOTTOM_BORDER | Rectangle.TOP_BORDER | Rectangle.RIGHT_BORDER;
-                        table.AddCell(cell);
-                    }
-                    cellIntern.AddElement(table);
-                }
-
-                PdfPCell cellLog = CreateCellWithParaTitle(5f, "log");
-                PdfPTable tableLog = depTable();
-
-                foreach (var tel in listLog)
-                {
-                    PdfPCell cell = new PdfPCell(new Phrase(tel.Name));
-                    tableLog.AddCell(cell);
-                    cell = new PdfPCell(new Phrase(tel.NumberIntern));
-                    tableLog.AddCell(cell);
-                }
-                cellLog.AddElement(tableLog);
-
-                PdfPCell cellGSM = CreateCellWithParaTitle(5f, "GSM");
-                PdfPTable tableGSM = depTable();
-
-                foreach (var tel in listGSM)
-                {
-                    PdfPCell cell = new PdfPCell(new Phrase(tel.Name));
-                    tableGSM.AddCell(cell);
-                    cell = new PdfPCell(new Phrase(tel.Number));
-                    tableGSM.AddCell(cell);
-                }
-                cellGSM.AddElement(tableGSM);
-
-
-                tblLeft.AddCell(cellIntern);
-                tblLeft.AddCell(cellLog);
-                PdfPCell nesthousing = new PdfPCell(tblLeft);
-                nesthousing.Padding = 0f;
-                layoutTable.AddCell(nesthousing);
-                layoutTable.AddCell(cellGSM);
-
-                document.Add(layoutTable);
-
-                document.Close();
-            }
-            catch (DocumentException de)
-            {
-                Console.Error.WriteLine(de.Message + "My DocumentException");
-            }
-            catch (IOException ioe)
-            {
-                Console.Error.WriteLine(ioe.Message + "My IOException");
-            }
-            document.Close();
-
-            stream.Flush();
-            stream.Position = 0;
-
-            return stream;
-        }
-        private PdfPCell CreateCell(float padding = 5f, string str = null)
-        {
-            var c = new PdfPCell();
-            if (!string.IsNullOrEmpty(str))
-            { c.Phrase = new Phrase(str); }
-            c.Padding = padding;
-            return c;
-        }
-        private PdfPCell CreateCellWithParaTitle(float padding, string str)
-        {
-            var c = CreateCell(padding);
-            if (!string.IsNullOrEmpty(str))
-            {
-                Paragraph para = new Paragraph();
-                para.Add(str);
-                para.SpacingAfter = 10f;
-                c.AddElement(para);
-            }
-            return c;
-        }
-        private PdfPTable depTable()
-        {
-            PdfPTable table = new PdfPTable(2);
-            table.WidthPercentage = 95f;
-            // table.SpacingBefore = 20f;
-            table.SpacingAfter = 10f;
-            return table;
-        }
-        // list of intern telephone numbers, no gsm's, without log department
-        private List<Group<string, TelephoneListItem>> TelephoneListByDepartment()
-        {
-            var currentPhones = db.UsePeriods
-           .Include(u => u.Asset)
-           .Include(u => u.Status)
-           .Include(u => u.UserAccount)
-           .Where(u => u.Asset is Telephone)
-           .Where(u => (u.Asset as Telephone).TelephoneType != "GSM")
-           .Where(u => u.EndDate == null || u.EndDate >= DateTime.Now)
-           .Where(u => u.UserAccount != null)
-           .Where(u => u.UserAccount.DepartmentString != "log")
-            .Select(u => new TelephoneListItem
-            {
-                Department = u.UserAccount.DepartmentString,
-                Name = u.UserAccount.Name,
-                NumberIntern = (u.Asset as Telephone).NumberIntern
-            });
-
-            var grouped1 = from phone in currentPhones
-                           group phone by phone.Department into groupedPhones
-                           orderby groupedPhones.Key
-                           select new Group<string, TelephoneListItem>
-                           { Key = groupedPhones.Key, Values = groupedPhones.OrderBy(t => t.Name) };
-            return grouped1.ToList();
-        }
-        private List<TelephoneListItem> TelephoneListLog(AssetManagerContext db)
-        {
-            var logPhones = db.UsePeriods
-           .Include(u => u.Asset)
-           .Include(u => u.Status)
-           .Include(u => u.UserAccount)
-           .Where(u => u.Asset is Telephone)
-           .Where(u => (u.Asset as Telephone).TelephoneType != "GSM")
-           .Where(u => u.EndDate == null || u.EndDate >= DateTime.Now)
-           .Where(u => u.UserAccount != null)
-           .Where(u => u.UserAccount.DepartmentString == "log")
-            .Select(u => new TelephoneListItem
-            {
-                Department = u.UserAccount.DepartmentString,
-                Name = u.UserAccount.Name,
-                NumberIntern = (u.Asset as Telephone).NumberIntern
-            });
-
-            return logPhones.ToList();
-        }
-        private List<TelephoneListItem> TelephoneListGSM(AssetManagerContext db)
-        {
-            var gsmPhones = db.UsePeriods
-           .Include(u => u.Asset)
-           .Include(u => u.Status)
-           .Include(u => u.UserAccount)
-           .Where(u => u.Asset is Telephone)
-           .Where(u => (u.Asset as Telephone).TelephoneType == "GSM")
-           .Where(u => u.EndDate == null || u.EndDate >= DateTime.Now)
-           .Where(u => u.UserAccount != null)
-            .Select(u => new TelephoneListItem
-            {
-                Department = u.UserAccount.DepartmentString,
-                Name = u.UserAccount.Name,
-                Number = (u.Asset as Telephone).Number
-            });
-
-            return gsmPhones.ToList();
-        }
+        }      
         protected override void Dispose(bool disposing)
         {
             if (disposing)
