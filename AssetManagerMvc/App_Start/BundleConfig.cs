552d8eaedf7289f6741aef3cb5f57cb14e222a0f
Hazy24 <hazy24@hotmail.com>
2016-01-20 15:52:20 +0100

    print css

--- AssetManagerMvc/App_Start/BundleConfig.cs
+++ AssetManagerMvc/App_Start/BundleConfig.cs
@@ -26,6 +26,10 @@ namespace AssetManagerMvc
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/print").Include(
+                     "~/Content/bootstrap.css",
+                     "~/Content/site.css",
+                     "~/Content/print.css"));
         }
     }
 }
